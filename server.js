32af7487768905f451b396a4f37a5414a0f62736
Owen Shepherd <414owen@gmail.com>
2017-03-11 23:50:29 +0000

    Server now saves small amount of history

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 #!/usr/bin/env node
 var ws = require('ws');
-
 function drawServ(wss) {
-	var lastPoint = "100,100,ccc";
+	const maxPoints = 20;
+	var lastPoints = ["100,100,ccc"];
 	const valid = /^-?\d+,-?\d+,[0-9a-f]{3}$/;
-
-	// Broadcast to all
 	wss.broadcast = function broadcast(data) {
 		wss.clients.forEach(function each(client) {
 			if (client.readyState === ws.OPEN) {
@@ -13,23 +11,22 @@ function drawServ(wss) {
 			}
 		});
 	};
-
-	// On message recieved
 	function incoming(data) {
-		// console.log("Recieved", data);
 		if (valid.test(data)) {
-			// console.log("Valid");
-			wss.broadcast("p" + data);
+			const point = "p" + data;
+			lastPoints.push(point);
+			if (lastPoints.length >= maxPoints) {
+				lastPoints.splice(0, lastPoints.length - maxPoints);
+			}
+			wss.broadcast(point);
 			lastPoint = data;
 		}
 	}
-
 	function updateUsers() { wss.broadcast("u" + wss.clients.size); }
-
-	// On new connection
 	wss.on('connection', function connection(ws) {
-		// console.log("Client connected");
-		ws.send("p" + lastPoint);
+		for (var i in lastPoints) {
+			ws.send(lastPoints[i]);
+		}
 		updateUsers();
 		ws.on('message', incoming);
 		ws.on('close', updateUsers);
